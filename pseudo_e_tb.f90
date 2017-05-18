5f46694326b4c7c3813e779b910f333737ce71c4
Wimalasiri <p470w698@submit1.hpc.crc.ku.edu>
2017-05-18 10:44:44 -0500

    edited pseudo_e_tb

--- pseudo_e_tb.f90
+++ pseudo_e_tb.f90
@@ -1,24 +1,29 @@
+!*****************************************************************************
 ! subroutine to calculate the pseudo potential for the solvated electron
 ! without the corresponding forces on the particle
-
-  subroutine pseudo_e_tb(n_atoms,rx,ry,rz,re,Lx,Ly,Lz,rcut)
+! Written by P.Wimalasiri and A.Katiyar
+! Hackathon Wednesday May 17, 2017 
+!******************************************************************************
+  subroutine pseudo_e_tb(re)
   use kinds
   use common_variables
+  use quantum_variables
+  implicit none
 
 ! scalars
-  real(kind=dp)    :: qo,qh,A1o,A1h,B1o,B1h,B2o,B2h,B3o,B3h,C1o
-  real(kind=dp)    :: vtmp
-  real(kind=dp),parameter :: alpha_pol=7.518E7,pi=3.41
-  integer(kind=ip) :: k
+  real(kind=dp)    :: qo,qh,A1o,A1h,B1o,B1h,B2o,B2h,B3o,B3h,C1o,pi
+  real(kind=dp)    :: doe,d1e,d2e,e1,e2,e3
+  real(kind=dp),parameter :: alpha_pol=7.518E7
+  integer(kind=ip) :: i,k
 
 ! arrays
-  real(kind=dp),allocatable,dimension(:,:) :: fpxi,fpyi,fpzi
+  real(kind=dp),allocatable,dimension(:,:) :: fg_ex,fg_ey,fg_ez
   real(kind=dp),dimension(3)    :: L,rO,rH1,rH2,rOe,r1e,r2e 
+  real(kind=dp), dimension(3), intent(in) :: re
 
-
-  allocate(fpxi(n_atoms,3))
-  allocate(fpyi(n_atoms,3))
-  allocate(fpzi(n_atoms,3))
+  allocate(fg_ex(n_atoms,3))
+  allocate(fg_ey(n_atoms,3))
+  allocate(fg_ez(n_atoms,3))
 
   qo  = -0.820
   qh  = 0.410
@@ -33,9 +38,11 @@
   C1o = 4.400*0.53
 
   vtmp = 0.0_dp
-  fpxi = 0.0_dp
-  fpyi = 0.0_dp
-  fpzi = 0.0_dp
+  fg_ex = 0.0_dp
+  fg_ey = 0.0_dp
+  fg_ez = 0.0_dp
+  
+  pi = 4d0*datan(1d0)
 
   do i = 1,n_atoms
 
@@ -78,61 +85,61 @@
         d1e = sqrt(d1e)
         d2e = sqrt(d2e)
 
-        if (dOe.le.rcut) then
+        if (dOe.le.r_cut) then
 ! calculate the polarization part
  
                 vtmp = vtmp - (0.5_dp*alpha/(dOe**2+C1o**2)**2)
 ! the force looks like
-                fpxi(i,1) = fpxi(i,1) + (2_dp*alpha/(rOe(1)**2+C1o**2)**3)
-                fpyi(i,1) = fpyi(i,2) + (2_dp*alpha/(rOe(2)**2+C1o**2)**3)
-                fpzi(i,1) = fpzi(i,3) + (2_dp*alpha/(rOe(3)**2+C1o**2)**3)
+                fg_ex(i,1) = fg_ex(i,1) + (2_dp*alpha/(rOe(1)**2+C1o**2)**3)
+                fg_ey(i,1) = fg_ey(i,2) + (2_dp*alpha/(rOe(2)**2+C1o**2)**3)
+                fg_ez(i,1) = fg_ez(i,3) + (2_dp*alpha/(rOe(3)**2+C1o**2)**3)
                 
 ! the electrostatics
 
 ! oxygen atom
   
-                e1 = erf(Alo*dOe)  
+                e1 = erf(A1o*dOe)  
                 e2 = erf(B2o*dOe)  
                 e3 = erf(B3o*dOe)  
   
                 vtmp = vtmp - (e1*qo/dOe + (e2 - e3)*B1o/dOe)
-                fpxi(i,1) = fpxi(i,1) + qo*rOe(1)*(e1/dOe**3             &
+                fg_ex(i,1) = fg_ex(i,1) + qo*rOe(1)*(e1/dOe**3             &
                  - (2_dp/sqrt(pi))*A1o*exp(-(A1o*dOe)**2)/dOe**2)      & 
                  + B1o*rOe(1)*(2_dp/sqrt(pi))*((B2o*exp(-(B2o*dOe)**2))&
                  - B3o*exp(-(B3o*dOe)**2))/dOe**2                        &
                  - (e2 - e3)/dOe**3 
 
-                fpyi(i,1) = fpxi(i,1) + qo*rOe(2)*(e1/dOe**3             &
+                fg_ey(i,1) = fg_ey(i,1) + qo*rOe(2)*(e1/dOe**3             &
                  - (2_dp/sqrt(pi))*A1o*exp(-(A1o*dOe)**2)/dOe**2)      &             
                  + B1o*rOe(1)*(2_dp/sqrt(pi))*((B2o*exp(-(B2o*dOe)**2))&
                  - B3o*exp(-(B3o*dOe)**2))/dOe**2                        &
                  - (e2 - e3)/dOe**3  
 
-                fpzi(i,1) = fpxi(i,1) + qo*rOe(3)*(e1/dOe**3             &
+                fg_ez(i,1) = fg_ez(i,1) + qo*rOe(3)*(e1/dOe**3             &
                  - (2_dp/sqrt(pi))*A1o*exp(-(A1o*dOe)**2)/dOe**2)      &             
                  + B1o*rOe(1)*(2_dp/sqrt(pi))*((B2o*exp(-(B2o*dOe)**2))&
                  - B3o*exp(-(B3o*dOe)**2))/dOe**2                        &
                  - (e2 - e3)/dOe**3  
 
 ! H1 atom
-                e1 = erf(Alh*d1e)
+                e1 = erf(A1h*d1e)
                 e2 = erf(B2h*d1e)
                 e3 = erf(B3h*d1e)
 
                 vtmp = vtmp - (e1*qh/d1e + (e2 - e3)*B1h/d1e)
-                fpxi(i,2) = fpxi(i,2) + qh*r1e(1)*(e1/d1e**3             &
+                fg_ex(i,2) = fg_ex(i,2) + qh*r1e(1)*(e1/d1e**3             &
                  - (2_dp/sqrt(pi))*A1h*exp(-(A1h*d1e)**2)/d1e**2)      &             
                  + B1h*r1e(1)*(2_dp/sqrt(pi))*((B2h*exp(-(B2h*d1e)**2))&
                  - B3h*exp(-(B3h*d1e)**2))/d1e**2                        &
                  - (e2 - e3)/d1e**3  
 
-                fpyi(i,2) = fpxi(i,2) + qh*r1e(2)*(e1/d1e**3             &
+                fg_ey(i,2) = fg_ey(i,2) + qh*r1e(2)*(e1/d1e**3             &
                  - (2_dp/sqrt(pi))*A1h*exp(-(A1h*d1e)**2)/d1e**2)      &
                  + B1h*r1e(1)*(2_dp/sqrt(pi))*((B2h*exp(-(B2h*d1e)**2))&
                  - B3h*exp(-(B3h*d1e)**2))/d1e**2                        &
                  - (e2 - e3)/d1e**3
 
-                fpzi(i,2) = fpxi(i,2) + qh*r1e(3)*(e1/d1e**3             &
+                fg_ez(i,2) = fg_ez(i,2) + qh*r1e(3)*(e1/d1e**3             &
                  - (2_dp/sqrt(pi))*A1h*exp(-(A1h*d1e)**2)/d1e**2)      &
                  + B1h*r1e(1)*(2_dp/sqrt(pi))*((B2h*exp(-(B2h*d1e)**2))&
                  - B3h*exp(-(B3h*d1e)**2))/d1e**2                        &
@@ -144,19 +151,19 @@
                 e3 = erf(B3h*d2e)
 
                 vtmp = vtmp - (e1*qh/d2e + (e2 - e3)*B1h/d2e)
-                fpxi(i,3) = fpxi(i,3) + qh*r2e(1)*(e1/d2e**3             &
+                fg_ex(i,3) = fg_ex(i,3) + qh*r2e(1)*(e1/d2e**3             &
                  - (2_dp/sqrt(pi))*A1h*exp(-(A1h*d2e)**2)/d2e**2)      &
                  + B1h*r1e(1)*(2_dp/sqrt(pi))*((B2h*exp(-(B2h*d2e)**2))&
                  - B3h*exp(-(B3h*d2e)**2))/d2e**2                        &
                  - (e2 - e3)/d2e**3
 
-                fpxi(i,3) = fpxi(i,3) + qh*r2e(2)*(e1/d2e**3             &
+                fg_ey(i,3) = fg_ey(i,3) + qh*r2e(2)*(e1/d2e**3             &
                  - (2_dp/sqrt(pi))*A1h*exp(-(A1h*d2e)**2)/d2e**2)      &
                  + B1h*r1e(1)*(2_dp/sqrt(pi))*((B2h*exp(-(B2h*d2e)**2))&
                  - B3h*exp(-(B3h*d2e)**2))/d2e**2                        &
                  - (e2 - e3)/d2e**3
 
-                fpxi(i,3) = fpxi(i,3) + qh*r2e(3)*(e1/d2e**3             &
+                fg_ez(i,3) = fg_ez(i,3) + qh*r2e(3)*(e1/d2e**3             &
                  - (2_dp/sqrt(pi))*A1h*exp(-(A1h*d2e)**2)/d2e**2)      &
                  + B1h*r1e(1)*(2_dp/sqrt(pi))*((B2h*exp(-(B2h*d2e)**2))&
                  - B3h*exp(-(B3h*d2e)**2))/d2e**2                        &

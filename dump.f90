a382ea64a122471a6aa76e37e22609a7bc8c1e9c
Mesele <o402m821@submit2.hpc.crc.ku.edu>
2017-05-18 16:48:29 -0500

    change to dump

--- dump.f90
+++ dump.f90
@@ -11,10 +11,10 @@
      write(nxyz,*) n_atoms
      write(nxyz,*) 'Atoms. Timestep: ', i
      do j=1,n_atoms
-        write(nxyz,'(I4.1,3F12.5)') elements(a_type(j)), x(j), y(j), z(j)
+        write(nxyz,'(A,3F12.5)') elements(a_type(j)), x(j), y(j), z(j)
      end do
      if (run_style .eq. 'qm_nvt' .or. run_style .eq. 'qm_nve') then
-        write(nxyz,'(I4.1,3F12.5)') 'I', r_e_avg(1), r_e_avg(2), r_e_avg(3)
+        write(nxyz,'(A,3F12.5)') 'I', r_e_avg(1), r_e_avg(2), r_e_avg(3)
      endif
 
 end subroutine dump

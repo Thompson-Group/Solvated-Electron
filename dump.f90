06ca227e277638f55ffffb0847da8ad0cc77f118
Piskulich <piskuliche@ku.edu>
2017-05-18 12:13:39 -0500

    updated dump

--- dump.f90
+++ dump.f90
@@ -10,7 +10,10 @@ integer(kind=ip) :: j
 	write(nxyz,*) n_atoms
 	write(nxyz,*) 'Atoms. Timestep: ', i
 	do j=1,n_atoms
-		write(nxyz,'(I4.1,3F12.5)') a_type(j), x(j), y(j), z(j)
+		write(nxyz,'(I4.1,3F12.5)') elements(a_type(j)), x(j), y(j), z(j)
 	end do
+    if (run_style .eq. 'qm_nvt' .or 'qm_nve') then
+        wrote(nxyz,'(I4.1,3F12.5)') 'I', r_e_avg(1), r_e_avg(2), r_e_avg(3)
+    endif
 
 end subroutine dump
